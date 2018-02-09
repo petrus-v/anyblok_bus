d6932591fbdb2745d6da6a0aa0e0c1180fdaf160
fjouatte <fjouatte@anybox.fr>
2018-02-09 11:06:49 +0100

    init consumer/producer/worker classes

--- anyblok_bus/bloks/bus/message.py
+++ anyblok_bus/bloks/bus/message.py
@@ -5,10 +5,18 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+
 from anyblok import Declarations
 from anyblok.column import Integer, String, Selection, LargeBinary
+from anyblok.config import Configuration
+from base64 import b64decode
+import logging
+import pika
+
+logger = logging.getLogger(__name__)
 
 Bus = Declarations.Model.Bus
+
 
 @Declarations.register(Bus)
 class Message:
@@ -18,40 +26,94 @@ class Message:
     id = Integer(primary_key=True)
     content_type = String(nullable=False)
     message = LargeBinary(nullable=False)
-
     type = Selection(
-            selections={
-                'consumer':'Consumer',
-                'producer':'Producer'
-            },
-            nullable=False)
+        selections={
+            'consumer': 'Consumer',
+            'producer': 'Producer'
+        },
+        nullable=False
+    )
+    # TODO : gérer l'ordre des messages par une séquence
 
     @classmethod
     def define_mapper_args(cls):
         mapper_args = super(Message, cls).define_mapper_args()
         if cls.__registry_name__ == 'Model.Bus.Message':
             mapper_args.update({'polymorphic_on': cls.type})
-        
+
         mapper_args.update({'polymorphic_identity': cls.MESSAGE_TYPE})
         return mapper_args
+
+    def get_profile(self, profile_name):
+        try:
+            profile = self.registry.Profile.query().filter_by(
+                name=profile_name
+            ).one()
+        except:
+            # TODO raise smthg or return
+            logger.error("Profile '{}' cannot be found.".format(profile_name))
+            raise Exception()
+        return profile
+
+    def init_connection(self, profile):
+        parameters = pika.URLParameters(profile.url)
+        return pika.BlockingConnection(parameters)
 
 
 @Declarations.register(Bus.Message)
 class Consumer(Bus.Message):
+
     MESSAGE_TYPE = 'consumer'
 
-    id = Integer(primary_key=True,
-            foreign_key=Bus.Message.use('id').options(ondelete='cascade'))
-
+    id = Integer(
+        primary_key=True,
+        foreign_key=Bus.Message.use('id').options(ondelete='cascade'),
+    )
     queue = String(nullable=False)
+
+    # TODO: add method to call when consuming ?
+
+    """
+    def receive(self, profile_name, callback):
+        profile = self.get_profile(profile_name)
+        _connection = self.init_connection(profile)
+        channel = _connection.channel()
+        channel.queue_declare(queue=self.queue)
+        channel.basic_consume(callback, queue=self.queue, no_ack=True)
+    """
+
 
 @Declarations.register(Bus.Message)
 class Producer(Bus.Message):
+
     MESSAGE_TYPE = 'producer'
 
-    id = Integer(primary_key=True,
-            foreign_key=Bus.Message.use('id').options(ondelete='cascade'))
-
+    id = Integer(
+        primary_key=True,
+        foreign_key=Bus.Message.use('id').options(ondelete='cascade'),
+    )
     exchange = String(nullable=False)
     routing_key = String(nullable=False)
 
+    def publish(self):
+        """ sending message to the exchange """
+        profile_name = Configuration.get('bus_profile')
+        profile = self.get_profile(profile_name)
+        message = b64decode(self.message)
+        _connection = self.init_connection(profile)
+        channel = _connection.channel()
+        channel.confirm_delivery()
+        if channel.basic_publish(
+            exchange=self.exchange,
+            routing_key=self.routing_key,
+            body=message,
+            properties=pika.BasicProperties(
+                content_type=self.content_type, delivery_mode=1
+            )
+        ):
+            logger.info("Message published %r", self)
+            self.unlink()
+            # if for obscure raison the message can be deleted
+            # then the message that dont break all
+        else:
+            raise Exception("Can publish message")
--- anyblok_bus/worker.py
+++ anyblok_bus/worker.py
@@ -5,17 +5,92 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+from anyblok.config import Configuration
+from anyblok_bus.bloks.bus.message import Message
 from logging import getLogger
+from pika import SelectConnection, URLParameters
 
 logger = getLogger(__name__)
 
 
 class Worker:
-    def __init__(self, registry):
+
+    def __init__(self, registry, profile):
         self.registry = registry
+        self.profile = profile
+        self._connection = None
+        self._channel = None
+        self._closing = False
+
+    def connect(self):
+        """ Creating connection object """
+        url = self.get_url()
+        logger.info('Connecting to %s', url)
+        return SelectConnection(
+            URLParameters(url),
+            self.on_connection_open,
+            stop_ioloop_on_close=False
+        )
+
+    def reconnect(self):
+        self._connection.ioloop.stop()
+        if not self._closing:
+            # TODO: use start() method ?
+            self._connection = self.connect()
+            self._connection.ioloop.start()
+
+    def get_url(self):
+        """ Retrieve connection url """
+        connection = self.profile
+        if connection:
+            return connection.url
+        raise Exception("Unknown profile")
+
+    def on_connection_open(self, *a):
+        """ Called when we are fully connected to RabbitMQ """
+        self._connection.add_on_close_callback(self.on_connection_closed)
+        self._connection.channel(on_open_callback=self.on_channel_open)
+        logger.info('Connexion opened')
+
+    def on_channel_open(self, channel):
+        """ Called when channel is opened """
+        logger.info('Channel opened')
+        self._channel = channel
+        self._channel.add_on_close_callback(self.on_channel_closed)
+        # TODO: adapter cette partie au fonctionnement souhaitée
+        # d'après la doc pika : on déclare un exchange, puis lorsque l'exchange est ok,
+        # on déclare une queue, lorsque la queue est ok on met en place lien entre la queue et l'exchange,
+        # enfin on démarre la "consommation" de la queue
+        for queue, model, method in self.registry.Consumer.query().all():
+            self.declare_consumer(queue, model, method)
+
+    def on_channel_closed(self, channel, reply_code, reply_text):
+        """ Called when channel is closed """
+        logger.warning(
+            'Channel %i was closed: (%s) %s', channel, reply_code, reply_text
+        )
+        self._connection.close()
+
+    def on_connection_closed(self, connection, reply_code, reply_text):
+        """ Called when connection is closed """
+        self._channel = None
+        if self._closing:
+            self._connection.ioloop.stop()
+        else:
+            logger.warning(
+                'Connection closed, reopening in 5 seconds: (%s) %s',
+                reply_code, reply_text
+            )
+            self._connection.add_timeout(5, self.reconnect)
+
+    def declare_consumer(self, queue, model, method):
+
 
     def start(self):
+        """ Creating connection object and starting event loop """
         logger.info('start')
+        self._connection = self.connect()
+        self._connection.ioloop.start()
 
     def stop(self):
-        logger.info('start')
+        logger.info('stop')
