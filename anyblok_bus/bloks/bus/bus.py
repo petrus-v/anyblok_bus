e428574bc02dbeed4701d5d62ddff0b8a00526b4
jssuzanne <jssuzanne@anybox.fr>
2018-03-01 15:52:31 +0100

    Add publishing high level method on Bus

--- anyblok_bus/bloks/bus/bus.py
+++ anyblok_bus/bloks/bus/bus.py
@@ -11,3 +11,28 @@ from anyblok import Declarations
 @Declarations.register(Declarations.Model)
 class Bus:
     """ Namespace Bus """
+
+    @classmethod
+    def publish(cls, exchange, routing_key, data, contenttype):
+        Message = cls.registry.Bus.Message.Producer
+        query = Message.query().order_by(Message.sequence.desc())
+        last_message = query.limit(1).one_or_none()
+        message = Message.insert(
+            exchange=exchange,
+            routing_key=routing_key,
+            content_type=contenttype,
+            message=data
+        )
+
+        if last_message:
+            message.sequence = last_message.sequence + 1
+
+        for m in Message.query().all():
+            try:
+                with cls.registry.begin_nested():  # savepoint
+                    message = Message.query().with_for_update(
+                        nowait=True).get(m.id)
+                    message.publish()
+
+            except:
+                pass
--- anyblok_bus/bloks/bus/message.py
+++ anyblok_bus/bloks/bus/message.py
@@ -9,7 +9,6 @@
 from anyblok import Declarations
 from anyblok.column import Integer, String, Selection, LargeBinary, Text
 from anyblok.config import Configuration
-from base64 import b64decode
 import logging
 import pika
 
@@ -45,27 +44,6 @@ class Message:
         mapper_args.update({'polymorphic_identity': cls.MESSAGE_TYPE})
         return mapper_args
 
-    def get_profile(self, profile_name):
-        try:
-            profile = self.registry.Bus.Profile.query().filter_by(
-                name=profile_name
-            ).one()
-        except:
-            # TODO raise smthg or return
-            logger.error("Profile '{}' cannot be found.".format(profile_name))
-            raise Exception()
-        return profile
-
-    def init_connection(self, profile):
-        parameters = pika.URLParameters(profile.url)
-        return pika.BlockingConnection(parameters)
-
-    def new_confirm_delivery_channel(self, profile):
-        _connection = self.init_connection(profile)
-        channel = _connection.channel()
-        channel.confirm_delivery()
-        return channel
-
 
 @Declarations.register(Bus.Message)
 class Consumer(Bus.Message):
@@ -93,23 +71,44 @@ class Producer(Bus.Message):
     exchange = String(nullable=False)
     routing_key = String(nullable=False)
 
+    def init_connection(self, profile):
+        parameters = pika.URLParameters(profile.url)
+        return pika.BlockingConnection(parameters)
+
     def publish(self):
         """ sending message to the exchange """
         profile_name = Configuration.get('bus_profile')
-        profile = self.get_profile(profile_name)
-        message = b64decode(self.message)
-        channel = self.new_confirm_delivery_channel(profile)
-        if channel.basic_publish(
-            exchange=self.exchange,
-            routing_key=self.routing_key,
-            body=message,
-            properties=pika.BasicProperties(
-                content_type=self.content_type, delivery_mode=1
-            )
-        ):
-            logger.info("Message published %r", self)
-            self.delete()
-            # if for obscure raison the message can be deleted
-            # then the message that dont break all
-        else:
-            raise Exception("Message cannot be published")
+        try:
+            with self.registry.begin_nested():  # savepoint
+                profile = self.registry.Bus.Profile.query().filter_by(
+                    name=profile_name
+                ).one_or_none()
+                _connection = self.init_connection(profile)
+                channel = _connection.channel()
+                channel.confirm_delivery()
+                if channel.basic_publish(
+                    exchange=self.exchange,
+                    routing_key=self.routing_key,
+                    body=self.message,
+                    properties=pika.BasicProperties(
+                        content_type=self.content_type, delivery_mode=1
+                    )
+                ):
+                    logger.info("Message published %r", self)
+                    self.delete()
+                    # if for obscure raison the message can be deleted
+                    # then the message that dont break all
+                else:
+                    raise Exception("Message cannot be published")
+        except Exception as e:
+            logger.error("publishing failed with : %r", e)
+            self.error = str(e)
+        finally:
+            if channel and not channel.is_closed and not channel.is_closing:
+                channel.close()
+            if (
+                _connection and
+                not _connection.is_closed and
+                not _connection.is_closing
+            ):
+                _connection.close()
